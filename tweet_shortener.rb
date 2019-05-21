ac09ec9a6e60e396d666c5f52ff3f77ebea8fd8c
Brittany Javalera <brittjavalera@gmail.com>
2019-05-21 21:38:13 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,6 +32,16 @@ end
 #   tweet
 # end
 
+#def word_substituter(tweet)
+#  tweet.split.collect do |word|
+#    if dictionary.keys.include?(word.downcase)
+#     word = dictionary[word.downcase]
+#    else
+#      word
+#    end
+#  end.join(" ")
+end
+
 
 def bulk_tweet_shortener(tweet_array)
   tweet_array.each do |tweet|
